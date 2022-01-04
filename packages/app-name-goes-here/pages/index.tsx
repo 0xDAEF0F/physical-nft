40c922c461da440b874a7b1d4e3dba1e288322e8
daemon <0xDAEF0F@proton.me>
2022-01-04 10:48:10 -0700

    Added Tailwind.

--- packages/app-name-goes-here/pages/index.tsx
+++ packages/app-name-goes-here/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div>Hello World</div>
+  return (
+    <div className='text-3xl font-bold bg-red-700 text-center'>Hello World</div>
+  )
 }
 
 export default Home
