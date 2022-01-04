811fac1a64e3cc690f093e5b77cf59c28c5b330e
daemon <0xDAEF0F@proton.me>
2022-01-04 11:24:55 -0700

    Added Redux.

--- packages/app-name-goes-here/__tests__/example.test.ts
+++ packages/app-name-goes-here/__tests__/example.test.ts
@@ -1,7 +1,7 @@
-import App from '@/pages/index';
+import App from '@/pages/index'
 
 describe('something', () => {
   it('sums', () => {
-    expect(2 + 2).toEqual(4);
-  });
-});
+    expect(2 + 2).toEqual(4)
+  })
+})
