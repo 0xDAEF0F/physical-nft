e7e8019bf4b2f45f83077dc761c461851e07b47e
daemon <0xDAEF0F@proton.me>
2022-01-04 22:09:56 -0700

    Refactored code.

---
+++ packages/app-name-goes-here/src/components/Logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <h1 className='font-bold text-xl cursor-pointer'>
+      Physical-<span className='text-blue-500'>NFT</span>
+    </h1>
+  )
+}
+
+export default Logo
