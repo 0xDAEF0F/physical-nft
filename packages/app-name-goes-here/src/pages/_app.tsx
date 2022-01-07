66cbf3d197f0c38b68614ed4dd5ee04958666b19
daemon <0xDAEF0F@proton.me>
2022-01-06 23:56:22 -0700

    Added firebase config and refactored code.

--- packages/app-name-goes-here/src/pages/_app.tsx
+++ packages/app-name-goes-here/src/pages/_app.tsx
@@ -2,11 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import store from 'src/app/store'
 import { Provider } from 'react-redux'
+import NavBar from '@/components/NavBar'
+import { Toaster } from 'react-hot-toast'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <NavBar />
       <Component {...pageProps} />
+      <Toaster />
     </Provider>
   )
 }
