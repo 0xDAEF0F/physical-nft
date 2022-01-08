ef7dad392bf06e91d192924b28139e2aef9a3eff
Alejandro M <amojarrot@gmail.com>
2022-01-07 18:39:10 -0700

    Added firebase emulators in development mode.

--- packages/app-name-goes-here/src/pages/index.tsx
+++ packages/app-name-goes-here/src/pages/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react'
 
+import { auth } from 'lib/firebase'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+
+const provider = new GoogleAuthProvider()
+
 export default function index() {
-  return <div>hello world</div>
+  function signIn() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return <div onClick={signIn}>sign in</div>
 }
