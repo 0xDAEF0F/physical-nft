5b28b67303e8bc7ea336edf88c6a2e158a303b10
dmojarrot <a01638460@itesm.mx>
2022-01-08 19:41:24 -0700

    Added BiddingCard and TrendingAuction

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { auth, googleProvider } from 'lib/firebase'
 import { signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth'
+import TrendingAuction from '@/components/TrendingAuctions'
 
 export default function index() {
-  return <div>hello world</div>
+  return (
+    <div>
+      <TrendingAuction />
+    </div>
+  )
 }
