52292e07a6251aac3948c47e40923a9beef919e3
Alejandro M <amojarrot@gmail.com>
2022-01-08 18:31:21 -0700

    try vercel.

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -3,5 +3,5 @@ import { auth, googleProvider } from 'lib/firebase'
 import { signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth'
 
 export default function index() {
-  return <div></div>
+  return <div>hello world</div>
 }
