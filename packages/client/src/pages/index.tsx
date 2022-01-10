1c8a89b1e0d2a55d06758a6c391e4d94fa757083
Alejandro M <amojarrot@gmail.com>
2022-01-09 22:26:25 -0700

    Implemented firebase in backend

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { auth, googleProvider } from 'lib/firebase'
+import { auth, firestore, googleProvider } from 'lib/firebase'
 import { signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth'
 import TrendingAuction from '@/components/TrendingAuctions'
 
