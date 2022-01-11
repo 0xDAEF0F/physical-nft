81d8e2a805b5aec8e6212bd7b3e999f8089c93b4
Alejandro M <amojarrot@gmail.com>
2022-01-10 22:57:30 -0700

    Implemented fetchUser and createUser in Firestore.

--- packages/client/src/pages/index.tsx
+++ packages/client/src/pages/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
-import { auth, firestore, googleProvider } from '@/lib/db/firebase-web'
-import { signInWithPopup, createUserWithEmailAndPassword } from 'firebase/auth'
 import TrendingAuction from '@/components/TrendingAuctions'
 import Footer from '@/components/Footer'
 
 export default function index() {
   return (
     <div>
-      <TrendingAuction />
+      {/* <TrendingAuction /> */}
       <Footer />
     </div>
   )
