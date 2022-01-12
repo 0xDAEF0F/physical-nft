8e87ff8d737f26e082037769cfcc2e24375e25ff
dmojarrot <a01638460@itesm.mx>
2022-01-11 23:07:04 -0700

    NavBar minor fixes

--- packages/client/src/components/NavBar.tsx
+++ packages/client/src/components/NavBar.tsx
@@ -5,28 +5,26 @@ import HamburgerMenu from './HamburgerMenu'
 
 export default function NavBar() {
   return (
-    <>
-      <div className='shadow-md sticky h-16'>
-        <nav className='flex h-full justify-between align-middle pl-5'>
-          <AppLogo extraClass={''} />
-          <SearchBar
-            placeholder='Search'
-            extraClass='hidden sm:flex px-10 xl:pl-40'
+    <div className='shadow-md sticky h-16'>
+      <nav className='flex h-full justify-between align-middle pl-5 items-center'>
+        <AppLogo extraClass={''} />
+        <SearchBar
+          placeholder='Search songs, albums, artists and accounts'
+          extraClass='hidden sm:flex px-10 xl:pl-40'
+        />
+        <ul className='hidden lg:flex w-5/12 justify-center'>
+          <DivLink to='/Explore' title='Explore' />
+          <DivLink to='/Stats' title='Stats' />
+          <DivLink
+            to='/create-account'
+            title='Get Started'
+            xClass='whitespace-nowrap'
           />
-          <ul className='items-center hidden lg:flex w-5/12 justify-center'>
-            <DivLink to='/Explore' title='Explore' />
-            <DivLink to='/Stats' title='Stats' />
-            <DivLink
-              to='/create-account'
-              title='Get Started'
-              xClass='whitespace-nowrap'
-            />
-          </ul>
-          <div className='p-6'>
-            <HamburgerMenu />
-          </div>
-        </nav>
-      </div>
-    </>
+        </ul>
+        <div className='pr-5'>
+          <HamburgerMenu />
+        </div>
+      </nav>
+    </div>
   )
 }
--- packages/client/src/components/SearchBar.tsx
+++ packages/client/src/components/SearchBar.tsx
@@ -8,19 +8,17 @@ type Props = {
 export default function SearchBar({ placeholder, extraClass }: Props) {
   return (
     <div className={`flex w-full items-center  ${extraClass}`}>
-      <div className='flex w-full justify-center'>
+      <div className='flex w-full justify-center items-center border rounded-lg'>
+        <SearchSvg
+          fontSize='25'
+          className='justify-center fill-gray-500 ml-2'
+        />
         <input
           type='text'
-          className='w-full px-2 py-2 text-base text-gray-700 transition focus:text-gray-700 focus:bg-white focus:outline-none border rounded-l-lg max-w-3xl'
+          className='w-full px-2 py-2 text-base text-gray-700 transition focus:text-gray-700 focus:bg-white focus:outline-none rounded-lg'
           placeholder={placeholder}
           aria-label='search'
         />
-        <button
-          className='px-4 py-3 bg-blue-600 text-white uppercase shadow-md hover:bg-black hover:shadow-lg focus:outline-none active:shadow-lg transition duration-150 rounded-r-lg'
-          type='button'
-        >
-          <SearchSvg fontSize='large' />
-        </button>
       </div>
     </div>
   )
