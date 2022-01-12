cf8d642e9105d970c6ee73d43e5a627d86415460
daemon <0xDAEF0F@proton.me>
2022-01-11 22:43:30 -0700

    Added functionality to sign message transaction.

--- packages/client/src/constants/index.ts
+++ packages/client/src/constants/index.ts
@@ -7,3 +7,12 @@ export type User = {
   username?: string
   email?: string
 }
+
+const customMessages = {
+  signNonceMessage:
+    'Please sign the following nonce to verify ownership of address.\nnonce: ',
+  metamaskNotInstalled: 'Please install Metamask extension.',
+  signedMessageFailed: 'Was not able to sign message.',
+}
+
+export { customMessages }
--- packages/client/src/pages/create-account.tsx
+++ packages/client/src/pages/create-account.tsx
@@ -1,14 +1,28 @@
 import { createFirestoreUser, getFirestoreUser } from '@/lib/firestore-helpers'
 import { useState } from 'react'
 import { dummyData } from 'src/mocks'
-import { PublicAddress, User } from '../constants'
-import { getDefaultProvider, ethers, providers } from 'ethers'
+import { Nonce, PublicAddress, User } from '../constants'
+import { getDefaultProvider, ethers, providers, Signer, Wallet } from 'ethers'
 import detectEthereumProvider from '@metamask/detect-provider'
 import { to } from 'await-to-js'
 import toast from 'react-hot-toast'
+import { customMessages } from '@/constants/index'
 
 export default function CreateAccount() {
   const [userObj, setUserObj] = useState<User>()
+
+  async function createProvider(): Promise<ethers.providers.Web3Provider> {
+    const ethereumWindowObject = await detectEthereumProvider()
+    return new Promise((res, rej) => {
+      if (!ethereumWindowObject) {
+        rej('Metamask not installed in browser.')
+      }
+      const provider = new providers.Web3Provider(
+        ethereumWindowObject as ethers.providers.ExternalProvider
+      )
+      res(provider)
+    })
+  }
 
   async function requestPublicAddressFromMetamask() {
     const ethereumWindowObject = await detectEthereumProvider()
@@ -21,6 +35,25 @@ export default function CreateAccount() {
     const [err, address] = await to(provider.getSigner().getAddress())
     if (err) throw new Error('Was not able to retrieve address, try again.')
     if (address) return address as PublicAddress
+  }
+
+  async function signNonce(nonce: Nonce) {
+    const [err, provider] = await to(createProvider())
+    if (!provider) {
+      console.error(err)
+      toast.error(customMessages.metamaskNotInstalled)
+      return
+    }
+    const signer = provider.getSigner()
+    const [err2, signedMessage] = await to(
+      signer.signMessage(customMessages.signNonceMessage + nonce)
+    )
+    if (!signedMessage) {
+      console.error(err2)
+      toast.error(customMessages.signedMessageFailed)
+      return
+    }
+    console.log(signedMessage)
   }
 
   async function createUser(user: User) {
@@ -45,7 +78,8 @@ export default function CreateAccount() {
 
   return (
     <div>
-      <button onClick={getUser}>Get Account</button>
+      {/* <button onClick={getUser}>Get Account</button> */}
+      <button onClick={() => signNonce(192387419237)}>Get Account</button>
       <div>
         <h1>user:</h1>
         <p>{'' || userObj?.email}</p>
