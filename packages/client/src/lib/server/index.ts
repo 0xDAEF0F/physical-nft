d0d8186432d9b1c037abfcc4ccfa5bc9727fee90
daemon <0xDAEF0F@proton.me>
2022-02-14 22:16:22 -0700

    Create phys-service-acct.json

--- packages/client/src/lib/server/index.ts
+++ packages/client/src/lib/server/index.ts
@@ -1,10 +1,17 @@
-import { getApp, getApps, initializeApp } from 'firebase-admin/app'
+import {
+  getApp,
+  getApps,
+  initializeApp,
+  cert,
+  ServiceAccount,
+} from 'firebase-admin/app'
 import { getFirestore } from 'firebase-admin/firestore'
 import { getAuth } from 'firebase-admin/auth'
+import serviceAccount from '../../../phys-service-acct.json'
 
 function createFirebaseAdminApp() {
   if (getApps().length === 0) {
-    return initializeApp()
+    return initializeApp({ credential: cert(serviceAccount as ServiceAccount) })
   } else {
     return getApp()
   }
