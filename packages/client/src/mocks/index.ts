3f7c23ae0188c64ee5f39424e1443678ea3188c8
daemon <0xDAEF0F@proton.me>
2023-06-16 11:31:14 -0600

    16_06_2023

--- packages/client/src/mocks/index.ts
+++ packages/client/src/mocks/index.ts
@@ -1,9 +1,3 @@
 import { User } from '@/constants/schema'
 
-export const dummyData: User[] = [
-  {
-    publicAddress: '0x8aC6Bd37D93f09cA5d1f0fc27ED4c0c72612f86c',
-    email: 'amojarrot@gmail.com',
-    username: 'daemon',
-  },
-]
+export const dummyData: User[] = []
